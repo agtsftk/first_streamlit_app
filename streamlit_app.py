dba58b5ef9dbfc9705c279af86f5daa2b2415df8
agtsftk <124212076+agtsftk@users.noreply.github.com>
2023-02-01 16:26:30 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,8 @@
 import streamlit
 
 streamlit.title('My Parents New Healtly Diner')
+
+streamlit.text('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
